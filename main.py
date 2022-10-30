48f8ddd965202c53e56a31cad0d32efbc20b05f2
915-Moga-Denis <moga_denis@yahoo.com>
2022-10-30 21:20:58 +0200

    Update main.py
    - Subtractions
    - Multiplications

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import auxiliary
 
-# TODO - additions(done), subtractions, multiplications and divisions by one digit
-# TODO - conversions by successive division, substitution method, rapid conversion
+# TODO - additions(done), subtractions(done), multiplications(done) and divisions by one digit
+# TODO - conversions by successive division(almost done), substitution method, rapid conversion
 
 
 def check_if_valid_base(number, base):
@@ -83,6 +83,168 @@ def add_numbers_in_a_base(number1, number2, base):
     return result
 
 
+def compare(number1, number2, base):
+    """
+
+    :param number1:
+    :param number2:
+    :param base:
+    :return:
+    """
+
+    if len(number1) < len(number2):
+        return True
+
+    elif len(number1) > len(number2):
+        return False
+
+    i = 0
+    j = 0
+
+    while i < len(number1) and j < len(number2):
+        digit1 = auxiliary.characters.index(number1[i])
+        digit2 = auxiliary.characters.index(number2[j])
+
+        if digit1 < digit2:
+            return True
+
+    return False
+
+
+def subtract_numbers_in_a_base(number1, number2, base):
+    """
+
+    :param number1:
+    :param number2:
+    :param base:
+    :return:
+    """
+
+    result = ""
+    swapped = False
+
+    if compare(number1, number2, base):
+        aux = number1
+        number1 = number2
+        number2 = aux
+        swapped = True
+
+    i = len(number1) - 1
+    j = len(number2) - 1
+
+    borrow = 0
+
+    while i >= 0 and j >= 0:
+        digit1 = auxiliary.characters.index(number1[i])
+        digit2 = auxiliary.characters.index(number2[j])
+
+        diff_of_digits = digit1 - digit2 - borrow
+
+        if diff_of_digits < 0:
+            new_digit = diff_of_digits + base
+            borrow = 1
+        else:
+            borrow = 0
+            new_digit = diff_of_digits
+
+        result = auxiliary.characters[new_digit] + result
+
+        i -= 1
+        j -= 1
+
+    while i >= 0:
+        digit = auxiliary.characters.index(number1[i])
+
+        diff_of_digits = digit - borrow
+
+        if diff_of_digits < 0:
+            new_digit = diff_of_digits + base
+            borrow = 1
+        else:
+            new_digit = diff_of_digits
+            borrow = 0
+
+        result = auxiliary.characters[new_digit] + result
+
+        i -= 1
+
+    while j >= 0:
+        digit = auxiliary.characters.index(number1[j])
+
+        diff_of_digits = digit - borrow
+
+        if diff_of_digits < 0:
+            new_digit = diff_of_digits + base
+            borrow = 1
+        else:
+            new_digit = diff_of_digits
+            borrow = 0
+
+        result = auxiliary.characters[new_digit] + result
+
+        j -= 1
+
+    redundant_zeroes = 0
+
+    while result[redundant_zeroes] == '0':
+        redundant_zeroes += 1
+
+    aux_result = result
+    result = ""
+
+    while redundant_zeroes < len(aux_result):
+        result += aux_result[redundant_zeroes]
+        redundant_zeroes += 1
+
+    if swapped:
+        result = '-' + result
+
+    return result
+
+
+def multiplication_with_one_digit(number, factor, base):
+    """
+
+    :param number:
+    :param factor:
+    :param base:
+    :return:
+    """
+
+    product = ""
+
+    decimal_factor = auxiliary.characters.index(factor)
+
+    negative = False
+
+    if number[0] == '-':
+        negative = True
+        number = number[1:]
+
+    i = len(number) - 1
+
+    carry = 0
+
+    while i >= 0:
+        digit = auxiliary.characters.index(number[i])
+
+        product_of_digits = digit * decimal_factor + carry
+
+        product = auxiliary.characters[product_of_digits % base] + product
+
+        carry = product_of_digits // base
+
+        i -= 1
+
+    if carry != 0:
+        product = auxiliary.characters[carry] + product
+
+    if negative:
+        product = '-' + product
+
+    return product
+
+
 def successive_divisions_conversion(number, final_base):
     """
 
@@ -97,18 +259,3 @@ def successive_divisions_conversion(number, final_base):
         number //= final_base
         result = auxiliary.characters[remainder] + result
     return result
-
-
-def rapid_conversions_method(number, initial_base, final_base):
-    """
-
-    :param number:
-    :param initial_base:
-    :param final_base:
-    :return:
-    """
-
-    conversions = {
-    #   10      2      4      8    16
-
-    }
